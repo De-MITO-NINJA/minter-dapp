6d17021ddc926d290bc4bd1e47d6dd3fa3d5c332
Mitowise <mitowise50@gmail.com>
2022-08-05 14:26:32 +0100

    date

--- backend/src/config.js
+++ backend/src/config.js
@@ -77,7 +77,7 @@ const TOKENS_PER_MINT = 5; // maximum number of NFTs a user can mint in a single
 const PUBLIC_MINT_START_DATE = "2022-08-08T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-08-05T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-08-06T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xD8C0326cf257D424c4a506F401eEa71Fb69aC3C4"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
