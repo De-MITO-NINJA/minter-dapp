6efe0da971b4321630d744f178f2bc110a09dc90
Mitowise <mitowise50@gmail.com>
2022-08-06 15:45:30 +0100

    price

--- backend/src/config.js
+++ backend/src/config.js
@@ -71,7 +71,7 @@ const OWNER_ADDRESS = '0xD8C0326cf257D424c4a506F401eEa71Fb69aC3C4';
 const TREASURY_ADDRESS = '0xD8C0326cf257D424c4a506F401eEa71Fb69aC3C4';
 const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
 const MINT_PRICE = 0.001; // Minting price per NFT. Rinkeby = ETH, Ethereum = ETH, Polygon = MATIC. CANNOT BE UPDATED!
-const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
+const TOKENS_PER_MINT = 5; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PUBLIC_MINT_START_DATE = "2022-08-08T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
