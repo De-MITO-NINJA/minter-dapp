fc0281825b08baf1f2987e1da220a853bdbe2b67
Mitowise <mitowise50@gmail.com>
2022-08-07 07:24:26 +0100

    nigga

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -285,7 +285,7 @@ function setTotalPrice() {
     mintInput.disabled = true;
     return;
   }
-  const totalPriceWei = BigInt(info.deploymentConfig.mintPrice) * BigInt(mintInputValue);
+  const totalPriceWei = log(info.deploymentConfig.mintPrice) * log(mintInputValue);
   
   let priceType = '';
   if(chain === 'rinkeby' || chain === 'ethereum') {
